604263923860abaf29c60bd31dd9c9e59f5682dd
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-04-13 17:57:39 +1000

    Algebra.Ring

---
+++ Data/Array/Accelerate/Algebra/Ring.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-- |
+-- Module      : Data.Array.Accelerate.Algebra.Ring
+-- Copyright   : [2016] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Data.Array.Accelerate.Algebra.Ring (
+
+  module Ring
+
+) where
+
+import Algebra.Ring                                       as Ring
+import qualified Data.Array.Accelerate.Algebra.Additive   as Additive
+
+import Data.Array.Accelerate                              as A
+import qualified Prelude                                  as P
+
+
+instance (Ring.C a, Additive.C a, IsNum a, Elt a) => C (Exp a) where
+  one           = constant one
+  (*)           = (P.*)
+  fromInteger x = constant (fromInteger x)
+
