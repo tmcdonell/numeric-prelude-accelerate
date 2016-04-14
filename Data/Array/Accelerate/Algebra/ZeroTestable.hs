2961a74bcc081d82550cdb9dbced1a76a893f599
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-04-14 18:22:00 +1000

    change type of ZeroTestable
    Constraints will now be of the form (ZeroTestable (Exp a)), which matches how the other class constrains will look.

--- Data/Array/Accelerate/Algebra/ZeroTestable.hs
+++ Data/Array/Accelerate/Algebra/ZeroTestable.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE FlexibleContexts     #-}
 {-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE NoImplicitPrelude    #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
 {-# LANGUAGE TypeOperators        #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 -- |
@@ -26,52 +28,60 @@ import qualified Prelude                                  as P
 
 
 class C a where
-  isZero :: Exp a -> Exp Bool         -- operand really doesn't need to be Exp
+  isZero :: a -> Exp Bool
 
-instance C Float  where isZero = defltIsZero
-instance C Double where isZero = defltIsZero
+instance C (Exp Float)  where isZero = defltIsZero
+instance C (Exp Double) where isZero = defltIsZero
 
-instance C Int    where isZero = defltIsZero
-instance C Int8   where isZero = defltIsZero
-instance C Int16  where isZero = defltIsZero
-instance C Int32  where isZero = defltIsZero
-instance C Int64  where isZero = defltIsZero
+instance C (Exp Int)    where isZero = defltIsZero
+instance C (Exp Int8)   where isZero = defltIsZero
+instance C (Exp Int16)  where isZero = defltIsZero
+instance C (Exp Int32)  where isZero = defltIsZero
+instance C (Exp Int64)  where isZero = defltIsZero
 
-instance C Word   where isZero = defltIsZero
-instance C Word8  where isZero = defltIsZero
-instance C Word16 where isZero = defltIsZero
-instance C Word32 where isZero = defltIsZero
-instance C Word64 where isZero = defltIsZero
+instance C (Exp Word)   where isZero = defltIsZero
+instance C (Exp Word8)  where isZero = defltIsZero
+instance C (Exp Word16) where isZero = defltIsZero
+instance C (Exp Word32) where isZero = defltIsZero
+instance C (Exp Word64) where isZero = defltIsZero
 
-instance C Z where
+instance C (Exp Z) where
   isZero _ = constant P.True
 
-instance (C sh, Slice sh) => C (sh :. Int) where
+instance (C (Exp sh), Slice sh) => C (Exp (sh :. Int)) where
   isZero sh = isZero (indexHead sh) &&* isZero (indexTail sh)
 
-instance (C a, C b, Elt a, Elt b) => C (a, b) where
-  isZero x = let (a,b) = unlift x in isZero a &&* isZero b
+instance (C (Exp a), C (Exp b), Elt a, Elt b) => C (Exp (a, b)) where
+  isZero x = let (a,b) = unlift x :: (Exp a, Exp b)
+             in  isZero a &&* isZero b
 
-instance (C a, C b, C c, Elt a, Elt b, Elt c) => C (a, b, c) where
-  isZero x = let (a,b,c) = unlift x in isZero a &&* isZero b &&* isZero c
+instance (C (Exp a), C (Exp b), C (Exp c), Elt a, Elt b, Elt c) => C (Exp (a, b, c)) where
+  isZero x = let (a,b,c) = unlift x :: (Exp a, Exp b, Exp c)
+             in  isZero a &&* isZero b &&* isZero c
 
-instance (C a, C b, C c, C d, Elt a, Elt b, Elt c, Elt d) => C (a, b, c, d) where
-  isZero x = let (a,b,c,d) = unlift x in isZero a &&* isZero b &&* isZero c &&* isZero d
+instance (C (Exp a), C (Exp b), C (Exp c), C (Exp d), Elt a, Elt b, Elt c, Elt d) => C (Exp (a, b, c, d)) where
+  isZero x = let (a,b,c,d) = unlift x :: (Exp a, Exp b, Exp c, Exp d)
+             in  isZero a &&* isZero b &&* isZero c &&* isZero d
 
-instance (C a, C b, C c, C d, C e, Elt a, Elt b, Elt c, Elt d, Elt e) => C (a, b, c, d, e) where
-  isZero x = let (a,b,c,d,e) = unlift x in isZero a &&* isZero b &&* isZero c &&* isZero d &&* isZero e
+instance (C (Exp a), C (Exp b), C (Exp c), C (Exp d), C (Exp e), Elt a, Elt b, Elt c, Elt d, Elt e) => C (Exp (a, b, c, d, e)) where
+  isZero x = let (a,b,c,d,e) = unlift x :: (Exp a, Exp b, Exp c, Exp d, Exp e)
+             in  isZero a &&* isZero b &&* isZero c &&* isZero d &&* isZero e
 
-instance (C a, C b, C c, C d, C e, C f, Elt a, Elt b, Elt c, Elt d, Elt e, Elt f) => C (a, b, c, d, e, f) where
-  isZero x = let (a,b,c,d,e,f) = unlift x in isZero a &&* isZero b &&* isZero c &&* isZero d &&* isZero e &&* isZero f
+instance (C (Exp a), C (Exp b), C (Exp c), C (Exp d), C (Exp e), C (Exp f), Elt a, Elt b, Elt c, Elt d, Elt e, Elt f) => C (Exp (a, b, c, d, e, f)) where
+  isZero x = let (a,b,c,d,e,f) = unlift x :: (Exp a, Exp b, Exp c, Exp d, Exp e, Exp f)
+             in  isZero a &&* isZero b &&* isZero c &&* isZero d &&* isZero e &&* isZero f
 
-instance (C a, C b, C c, C d, C e, C f, C g, Elt a, Elt b, Elt c, Elt d, Elt e, Elt f, Elt g) => C (a, b, c, d, e, f, g) where
-  isZero x = let (a,b,c,d,e,f,g) = unlift x in isZero a &&* isZero b &&* isZero c &&* isZero d &&* isZero e &&* isZero f &&* isZero g
+instance (C (Exp a), C (Exp b), C (Exp c), C (Exp d), C (Exp e), C (Exp f), C (Exp g), Elt a, Elt b, Elt c, Elt d, Elt e, Elt f, Elt g) => C (Exp (a, b, c, d, e, f, g)) where
+  isZero x = let (a,b,c,d,e,f,g) = unlift x :: (Exp a, Exp b, Exp c, Exp d, Exp e, Exp f, Exp g)
+             in  isZero a &&* isZero b &&* isZero c &&* isZero d &&* isZero e &&* isZero f &&* isZero g
 
-instance (C a, C b, C c, C d, C e, C f, C g, C h, Elt a, Elt b, Elt c, Elt d, Elt e, Elt f, Elt g, Elt h) => C (a, b, c, d, e, f, g, h) where
-  isZero x = let (a,b,c,d,e,f,g,h) = unlift x in isZero a &&* isZero b &&* isZero c &&* isZero d &&* isZero e &&* isZero f &&* isZero g &&* isZero h
+instance (C (Exp a), C (Exp b), C (Exp c), C (Exp d), C (Exp e), C (Exp f), C (Exp g), C (Exp h), Elt a, Elt b, Elt c, Elt d, Elt e, Elt f, Elt g, Elt h) => C (Exp (a, b, c, d, e, f, g, h)) where
+  isZero x = let (a,b,c,d,e,f,g,h) = unlift x :: (Exp a, Exp b, Exp c, Exp d, Exp e, Exp f, Exp g, Exp h)
+             in  isZero a &&* isZero b &&* isZero c &&* isZero d &&* isZero e &&* isZero f &&* isZero g &&* isZero h
 
-instance (C a, C b, C c, C d, C e, C f, C g, C h, C i, Elt a, Elt b, Elt c, Elt d, Elt e, Elt f, Elt g, Elt h, Elt i) => C (a, b, c, d, e, f, g, h, i) where
-  isZero x = let (a,b,c,d,e,f,g,h,i) = unlift x in isZero a &&* isZero b &&* isZero c &&* isZero d &&* isZero e &&* isZero f &&* isZero g &&* isZero h &&* isZero i
+instance (C (Exp a), C (Exp b), C (Exp c), C (Exp d), C (Exp e), C (Exp f), C (Exp g), C (Exp h), C (Exp i), Elt a, Elt b, Elt c, Elt d, Elt e, Elt f, Elt g, Elt h, Elt i) => C (Exp (a, b, c, d, e, f, g, h, i)) where
+  isZero x = let (a,b,c,d,e,f,g,h,i) = unlift x :: (Exp a, Exp b, Exp c, Exp d, Exp e, Exp f, Exp g, Exp h, Exp i)
+             in  isZero a &&* isZero b &&* isZero c &&* isZero d &&* isZero e &&* isZero f &&* isZero g &&* isZero h &&* isZero i
 
 
 -- | Checks if a number is the zero element. This test is not possible for all
@@ -83,6 +93,6 @@ instance (C a, C b, C c, C d, C e, C f, C g, C h, C i, Elt a, Elt b, Elt c, Elt 
 -- values (the unit of a zero quantity is unknown), residue class (the modulus
 -- is unknown).
 --
-defltIsZero :: (Additive.C a, IsNum a, Elt a) => Exp a -> Exp Bool
+defltIsZero :: (Additive.C (Exp a), IsNum a, Elt a) => Exp a -> Exp Bool
 defltIsZero = (Additive.zero ==*)
 
