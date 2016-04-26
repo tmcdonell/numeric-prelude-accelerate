2b0d3b2def5177ecdf87232e6535852a2c87a6ed
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-04-26 16:05:39 +1000

    add instance ZeroTestable Complex

--- Data/Array/Accelerate/Number/Complex.hs
+++ Data/Array/Accelerate/Number/Complex.hs
@@ -48,18 +48,21 @@ module Data.Array.Accelerate.Number.Complex (
 
 ) where
 
-import qualified Number.Complex                           as Complex
+import qualified Number.Complex                                     as Complex
 
--- import qualified Data.Array.Accelerate.Algebra.Absolute   as Absolute
-import qualified Data.Array.Accelerate.Algebra.Additive   as Additive
-import qualified Data.Array.Accelerate.Algebra.Ring       as Ring
+import qualified Data.Array.Accelerate.Algebra.Absolute             as Absolute
+import qualified Data.Array.Accelerate.Algebra.Additive             as Additive
+import qualified Data.Array.Accelerate.Algebra.Algebraic            as Algebraic
+import qualified Data.Array.Accelerate.Algebra.Field                as Field
+import qualified Data.Array.Accelerate.Algebra.Ring                 as Ring
+import qualified Data.Array.Accelerate.Algebra.ZeroTestable         as ZeroTestable
 
-import Data.Array.Accelerate                              as A
+import Data.Array.Accelerate                                        as A hiding ( signum )
 import Data.Array.Accelerate.Smart
 import Data.Array.Accelerate.Product
 import Data.Array.Accelerate.Array.Sugar
 
-import Prelude                                            ( ($), undefined )
+import Prelude                                                      ( ($), (.), undefined )
 
 #if __GLASGOW_HASKELL__ <= 708
 import Data.Typeable
@@ -208,6 +211,9 @@ instance (Ring.C (Exp a), Elt a) => Ring.C (Exp (Complex.T a)) where
 --   abs    = lift1 (Absolute.abs    :: Complex.T (Exp a) -> Complex.T (Exp a))
 --   signum = lift1 (Absolute.signum :: Complex.T (Exp a) -> Complex.T (Exp a))
 
+instance (ZeroTestable.C (Exp a), Elt a) => ZeroTestable.C (Exp (Complex.T a)) where
+  isZero c = ZeroTestable.isZero (real c)
+         &&* ZeroTestable.isZero (imag c)
 
 -- Accelerate classes
 -- ------------------
