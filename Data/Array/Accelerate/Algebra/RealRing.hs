1d3b2f1c25f495f0acda98f969b1373a8eac67dc
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-05-23 17:30:16 +1000

    add [empty] Algebra.RealRing
    The types don't work for us, but we require this to fulfil superclass constrainst.

---
+++ Data/Array/Accelerate/Algebra/RealRing.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-- |
+-- Module      : Data.Array.Accelerate.Algebra.RealRing
+-- Copyright   : [2016] Trevor L. McDonell
+-- License     : BSD3
+--
+-- Maintainer  : Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
+module Data.Array.Accelerate.Algebra.RealRing (
+
+  module RealRing
+
+) where
+
+import Algebra.RealRing                                             as RealRing ( C(..) )
+import Data.Array.Accelerate.Algebra.Additive                       as Additive
+import Data.Array.Accelerate.Algebra.ToInteger                      as ToInteger
+-- import Data.Array.Accelerate.Algebra.Ring                           as Ring
+-- import Data.Array.Accelerate.Algebra.Absolute                       as Absolute
+
+import Data.Array.Accelerate                                        hiding ( (+), (-), (*), fromIntegral )
+-- import qualified Data.Array.Accelerate                              as A
+
+-- import Prelude                                                      ( (.) )
+
+-- import Data.Array.Accelerate                                        as A ( Exp, Elt, Ord, lift, (>=*), (?) )
+
+-- These are likely to fail as they depend on ToInteger.fromIntegral, which only
+-- makes sense if GHC rewrite rules fire.
+--
+-- See [ToIntegral and rewrite rules].
+
+instance RealRing.C (Exp Int) where
+  splitFraction x = (fromIntegral x, zero)
+  fraction      _ = zero
+  floor           = fromIntegral
+  ceiling         = fromIntegral
+  round           = fromIntegral
+  truncate        = fromIntegral
+
+instance RealRing.C (Exp Float)
+--   floor           = A.floor
+--   ceiling         = A.ceiling
+--   round           = A.round
+--   truncate        = A.truncate
+
+instance RealRing.C (Exp Double)
+
+
+
+-- div' n d = A.floor (n / d)
+
+-- mod' n d = n - (fromIntegral f) * d
+--   where
+--       f :: Exp Int64
+--       f = div' n d
+
+
+-- fastFraction :: (Ord t, Ring.C (Exp t)) => Exp t -> Exp t
+-- fastFraction x
+--   = (minBound :: Exp Int64) <=* x &&* x <=* (maxBound :: Exp Int64)
+--   ? ( n - A.toFloating f * d
+--     , zero -- error...
+--     )
+  -- This is only valid when minBound <= x <= maxBound
+  -- fixFraction (x - trunc x)
+
+-- fixFraction :: (Ord t, Ring.C (Exp t)) => Exp t -> Exp t
+-- fixFraction x =
+--   x >=* zero ? ( x, x + one )
+
+
+-- fastSplitFraction
+--     :: forall a b. (Ring.C (Exp b), Absolute.C (Exp a), Ord a, Elt a, Elt b)
+--     => (Exp a -> Exp Int)
+--     -> (Exp Int -> Exp a)
+--     -> Exp a
+--     -> Exp (b,a)
+-- fastSplitFraction trunc toFloat x =
+--   let n = trunc x
+--       f = x - toFloat n
+--       i = fromIntegral n  :: Exp b
+--   in
+--   f >=* zero ? ( lift (i,     f)     :: Exp (b,a)
+--                , lift (i-one, f+one) :: Exp (b,a)
+--                )
+
